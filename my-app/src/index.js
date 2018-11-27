6b9659b2058bf5f72fc84f035396c9657d65df5d
Sarthak Batra <sarthakbatra1993@gmail.com>
2018-11-27 13:13:28 -0800

    Component 'Board' controlling state of children components 'Square'

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
   }
+}
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+    
+    handleClick(i) {
+      // Use .slice() to create a copy instead of modifying the existing array
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
